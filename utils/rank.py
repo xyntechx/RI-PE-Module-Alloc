dc2e8eb2873299684445a5c1432a6d5f5dee3e6b
xyntechx <xyntechx@gmail.com>
2023-03-20 07:33:38 +0800

    ✨ Convert int to rank

---
+++ utils/rank.py
@@ -0,0 +1,16 @@
+def index_to_rank(index):
+    match index:
+        case 1:
+            return "1st"
+        case 2:
+            return "2nd"
+        case 3:
+            return "3rd"
+        case 4:
+            return "4th"
+        case 5:
+            return "5th"
+        case 6:
+            return "6th"
+        case 7:
+            return "7th"
