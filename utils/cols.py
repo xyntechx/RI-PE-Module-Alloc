847c65ef7671b9f03b6d7052605fe7957fa882ad
xyntechx <xyntechx@gmail.com>
2023-03-20 07:33:27 +0800

    ✨ Map col names to keys

---
+++ utils/cols.py
@@ -0,0 +1,9 @@
+NAME = "Name"
+
+CLASS = "Class"
+
+GENDER = "Q2 - GENDER"
+
+REMARKS_1 = "Remarks"
+
+REMARKS_2 = "Q3 - Please let us know if you have any medical conditions / modules that you are unable to participate in based on the choices given. Choose only those you can do (if available), Please also remember to send in your relevant doctor's letter or equivalent as stated at the start of this Form. (NA - if not applicable)"
