d0ea247df13bdbd6dec99d7555acae9824b5d024
xyntechx <xyntechx@gmail.com>
2023-03-24 14:52:17 +0800

    ✨ New columns

--- utils/cols.py
+++ utils/cols.py
@@ -4,6 +4,16 @@ CLASS = "Class"
 
 GENDER = "Q2 - GENDER"
 
+GENDER_F = "Gender"
+
 REMARKS_1 = "Remarks"
 
 REMARKS_2 = "Q3 - Please let us know if you have any medical conditions / modules that you are unable to participate in based on the choices given. Choose only those you can do (if available), Please also remember to send in your relevant doctor's letter or equivalent as stated at the start of this Form. (NA - if not applicable)"
+
+MODULE = "Allocated Module"
+
+VENUE = "Venue"
+
+TEACHER = "Teacher"
+
+TEACHER_I = "Teacher Initials"
